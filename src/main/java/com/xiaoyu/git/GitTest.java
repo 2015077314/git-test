8fa55be95ad8f7f6a6cde159a12765bcb63173d8
xiaoyu <2015077314@qq.com>
2022-10-12 16:41:35 +0800

    push commit

--- src/main/java/com/xiaoyu/git/GitTest.java
+++ src/main/java/com/xiaoyu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello world");
         System.out.println("hello git4!");
+        System.out.println("我修改了代码，你快push到github上吧");
     }
 }
